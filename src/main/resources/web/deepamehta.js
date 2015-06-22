488f009afaf956c4e126596bef630ccc01dffe4f
Jörg Richter <jri@deepamehta.de>
2015-06-23 00:59:23 +0200

    Render composite topics. NO FINAL SUCESS.

--- src/main/resources/web/deepamehta.js
+++ src/main/resources/web/deepamehta.js
@@ -1,16 +1,31 @@
 DM4 = {
 
+    Object: function(object) {
+        if (object) {
+            this.id      = object.id;
+            this.uri     = object.uri;
+            this.typeUri = object.type_uri;
+            this.value   = object.value;
+            this.childs  = DM4.buildChildTopics(object.childs);
+        }
+    },
+
+    Topic: function(topic) {
+        DM4.Object.call(this, topic);
+    },
+
+    Association: function(assoc) {
+        DM4.Object.call(this, assoc);
+        this.role1 = assoc.role_1;
+        this.role2 = assoc.role_2;
+    },
+
     Type: function(type) {
+        DM4.Topic.call(this, type);
         if (type) {
-            this.id      = type.id;
-            this.uri     = type.uri;
-            this.value   = type.value;
-            this.typeUri = type.type_uri;
-            this.childs  = type.childs;
-            //
             this.dataTypeUri      = type.data_type_uri;
             this.indexModeUris    = type.index_mode_uris;
-            this.assocDefs        = type.assoc_defs;
+            this.assocDefs        = DM4._initAssocDefs(type.assoc_defs);
             this.labelConfig      = type.label_config;
             this.viewConfigTopics = type.view_config_topics;
         }
@@ -22,6 +37,48 @@ DM4 = {
 
     AssociationType: function(assocType) {
         DM4.Type.call(this, assocType);
+    },
+
+    // --- Utilities ---
+
+    buildChildTopics: function(childs) {
+        // console.log("DM4.buildChildTopics", childs);
+        var childTopics = {}
+        for (var childTypeUri in childs) {
+            var childTopic = childs[childTypeUri]
+            if (js.is_array(childTopic)) {
+                childTopics[childTypeUri] = []
+                for (var i = 0, topic; topic = childTopic[i]; i++) {
+                    childTopics[childTypeUri].push(new Topic(topic))
+                }
+            } else {
+                childTopics[childTypeUri] = new Topic(childTopic)
+            }
+        }
+        return childTopics
+    },
+
+    _initAssocDefs: function(assocDefs) {
+        for (var i = 0, assocDef; assocDef = assocDefs[i]; i++) {
+            // add convenience properties
+            var assoc = new DM4.Association(assocDef);
+            assocDef.parentTypeUri = assoc.getRole("dm4.core.parent_type").topic_uri;
+            assocDef.childTypeUri  = assoc.getRole("dm4.core.child_type").topic_uri;
+        }
+        return assocDefs;
+    }
+}
+
+DM4.Association.prototype = {
+
+    getRole: function(roleTypeUri) {
+        var match1 = this.role1.role_type_uri == roleTypeUri;
+        var match2 = this.role2.role_type_uri == roleTypeUri;
+        if (match1 && match2) {
+            throw "AssociationError: ambiguity in association " + this.id +
+                " when looking for the \"" + roleTypeUri + "\" role";
+        }
+        return match1 ? this.role1 : match2 ? this.role2 : undefined;
     }
 }
 
